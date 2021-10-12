cd8e3ef44bc6dd411392ab6f63f271b1fba8f0c8
Wijayaac <wijayaac25@gmail.com>
2021-10-12 14:20:16 +0800

    🎉 online store projects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/style.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.bundle') : null
+  }, [])
+  return <>
+
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp
